ec5fd64fa3fe7adddc49c202c15519a3fed65ef2
Andrew Sprague <andrewsprague@outlook.com>
2018-08-15 00:59:11 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (arr)
+  # code hee
+  i = 0
+  while i < arr.length do
+    yield arr[i]
+    i += 1
+  end
+end
+
+my_each(arr) do |i|
+  puts i
 end
